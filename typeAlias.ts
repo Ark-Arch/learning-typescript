092548af6d901da890b81bb7bea7d063a1e99357
Agbemuko David Ifeoluwa <davidagbemuko.ad@gmail.com>
2025-04-07 08:49:24 +0100

    writing interfaces

--- typeAlias.ts
+++ typeAlias.ts
@@ -7,3 +7,12 @@ type User = {
 function createUser(user: User): User {
     return user
 }
+
+interface Human {
+    name: string;
+    age: number;
+}
+
+interface Child extends Human {
+    height: string
+}
